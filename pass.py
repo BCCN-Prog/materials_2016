698587e63d78f21466538e3c09066d2104c3450c
David Higgins (TUPro) <dave@uiginn.com>
2016-02-22 15:26:27 +0100

    Pause for debugging. We can run this version but we don’t have a pickle generated file to read from.

--- pass.py
+++ pass.py
@@ -1,10 +1,33 @@
+import pickle
 from getpass import getpass
 
-username = input('Enter your username: ')
-password = getpass('Enter your password: ')
+def read_pswdb(pswdb_file):
+    pswdb = pickle.load(pswdb_file)
+    return pswdb
 
-pswdb = open('pswdb', 'wb')
+def write_pswdb(pswdb, pswdb_file):
+    pickle.dump(pswdb, pswdb_file)
+
+def get_credentials():
+    username = input('Enter your username: ')
+    password = getpass('Enter your password: ')
+    return (username, password)
+
+def authorize(username, password, pswdb):
+    if username in pswdb:
+        if password == pswdb[username]:
+            return True
+    return False
+
+
+pswdb_file = open('pswdb', 'rb+')
+username, password = get_credentials()
+pswdb = read_pswdb(pswdb_file)
+if authorize(username, password, pswdb):
+    print('Authorization succeeded!')
+else:
+    print('Wrong username or password')
+
 #pswdb.write(username+' '+password+'\n')
 #pswdb.close()
-pswdb[username] = password
-pickle.dump(pswdb, pswdb_file)
+#pswdb[username] = password
