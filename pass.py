30f54feb50c641c8a7fb7b0dfedc39fc72f879b9
David Higgins (TUPro) <dave@uiginn.com>
2016-02-22 15:39:19 +0100

    First working version.
    We check our pickle’d database of usernames and passwords. If there is no matching entry we add a new one. Authorisation is printed (or denied) and the password database is saved to the pickle file on disk.

--- pass.py
+++ pass.py
@@ -2,7 +2,10 @@ import pickle
 from getpass import getpass
 
 def read_pswdb(pswdb_file):
-    pswdb = pickle.load(pswdb_file)
+    try:
+        pswdb = pickle.load(pswdb_file)
+    except EOFError:
+        pswdb = {}
     return pswdb
 
 def write_pswdb(pswdb, pswdb_file):
@@ -14,20 +17,25 @@ def get_credentials():
     return (username, password)
 
 def authorize(username, password, pswdb):
+    print(username, password)
     if username in pswdb:
         if password == pswdb[username]:
             return True
+    print(pswdb)
     return False
 
+def create_new_user(username, password, paswdb, pswdb_file):
+    pswdb[username] = password
+    write_pswdb(pswdb, pswdb_file)
 
 pswdb_file = open('pswdb', 'rb+')
+
+
+
 username, password = get_credentials()
 pswdb = read_pswdb(pswdb_file)
 if authorize(username, password, pswdb):
     print('Authorization succeeded!')
 else:
+    create_new_user(username, password, pswdb, pswdb_file)
     print('Wrong username or password')
-
-#pswdb.write(username+' '+password+'\n')
-#pswdb.close()
-#pswdb[username] = password
