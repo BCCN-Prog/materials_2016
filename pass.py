983ee313937bea0442b4d0bda630aed894cf1f74
David Higgins (TUPro) <dave@uiginn.com>
2016-02-22 15:11:04 +0100

    First attempt using pickle.

--- pass.py
+++ pass.py
@@ -2,3 +2,9 @@ from getpass import getpass
 
 username = input('Enter your username: ')
 password = getpass('Enter your password: ')
+
+pswdb = open('pswdb', 'wb')
+#pswdb.write(username+' '+password+'\n')
+#pswdb.close()
+pswdb[username] = password
+pickle.dump(pswdb, pswdb_file)
