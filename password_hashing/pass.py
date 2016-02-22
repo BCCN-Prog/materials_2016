78f58b45bee77afcbf46071fd4b376f7957d03d4
Tiziano Zito <opossumnano@gmail.com>
2016-02-22 15:45:27 +0100

    first version of the password script

---
+++ password_hashing/pass.py
@@ -0,0 +1,42 @@
+import pickle
+from getpass import getpass
+
+def read_pswdb(pswdb_file):
+    try:
+        pswdb = pickle.load(pswdb_file)
+    except EOFError:
+        pswdb = {}
+    return pswdb
+
+def write_pswdb(pswdb, pswdb_file):
+    pickle.dump(pswdb, pswdb_file)
+
+def get_credentials():
+    username = input('Enter your username: ')
+    password = getpass('Enter your password: ')
+    return (username, password)
+
+def authorize(username, password, pswdb):
+    print(username, password)
+    if username in pswdb:
+        if password == pswdb[username]:
+            return True
+    print(pswdb)
+    return False
+
+def create_new_user(username, password, pswdb, pswdb_file):
+    pswdb[username] = password
+    write_pswdb(pswdb, pswdb_file)
+
+pswdb_file = open('pswdb', 'rb+')
+
+
+username, password = get_credentials()
+pswdb = read_pswdb(pswdb_file)
+
+if authorize(username, password, pswdb):
+    print('Authorization succeeded!')
+else:
+    create_new_user(username, password, pswdb, pswdb_file)
+    print('Wrong username or password. Added to the database!')
+
