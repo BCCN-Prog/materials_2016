c40a7797633c71f1233f47c90ff6859427ac5c36
christiane <christiane.wiebel@tu-berlin.de>
2016-02-23 04:12:57 -0500

    added first test- failed

--- eren_christiane/test_maxima.py
+++ eren_christiane/test_maxima.py
@@ -1,5 +1,14 @@
 from maxima import find_maxima
 
-x = [1,2,3,2,4,3]
-print(find_maxima(x))
+def test_simple_sequence():
+	exp = [2,4]
+	x   = [1,2,3,2,4,3]
+	out = find_maxima(x)
+	assert exp == out
+
+def test_shorter_sequence():
+	exp = [2]	
+	x = [1,2,3]
+	out = find_maxima(x)
+	assert exp ==out
 
