40f5ff5d14755d2348a369630f214987befeea24
Eren Sezener <erensezener@gmail.com>
2016-02-23 14:56:45 +0100

    final version of test_maxima

--- testing/eren_christiane/maxima.py
+++ testing/eren_christiane/maxima.py
@@ -1,6 +1,25 @@
 # Advanced Scientific Programming in Python
 # Exercise 3
 
+import numpy as np
+
+
+def check_maxima(d, negative_indices):
+    maximas = []
+    for i in negative_indices:
+        index_before = i-1
+        if d[index_before] > 0:
+            maximas.append(i)
+        if d[index_before] < 0:
+            continue
+        while d[index_before] == 0 and index_before>1:
+            index_before -= 1
+            if d[index_before] > 0:
+                maximas.append(i)
+            if d[index_before] < 0:
+                break
+    return maximas
+
 
 def find_maxima(x):
     """Find local maxima of x.
@@ -21,25 +40,16 @@ def find_maxima(x):
         message = 'Input argument must be a list, got %d instead' % type(x)
         raise TypeError(message)
 
-    idx = []
-    
     if len(x) == 1:
         return [0]
 
-    if x[0] > x[1]:
-        idx.append(0)
-
-    if x[-1] > x[-2]:
-        idx.append(len(x) - 1)
-
-    for j in range(len(x)-2):
-        # `i` is a local maximum if the signal decreases before and after it
-        i = j + 1
-        if x[i-1] < x[i] and x[i+1] < x[i]:
-            idx.append(i)
-    return idx
-
-    # NOTE for the curious: the code above could be written using
-    # list comprehension as
-    # return [i for i in range(len(x)) if x[i-1]<x[i] and x[i+1]<x[i]]
-    # not that this would solve the bugs ;-)
+    d = np.hstack((0,np.diff(x)))
+    negative_indices = np.where(d<0)[0]
+    # is_maxima = (negative_indices - 1 >= 0)
+    maxima = (np.array(check_maxima(d, negative_indices)) -1).tolist()
+    # maxima = (maxima - 1).tolist()
+    if d[-1] > 0:
+        maxima.append(len(x)-1)
+    if d[1] < 0:
+        maxima.append(0)
+    return sorted(maxima)
--- testing/eren_christiane/test_maxima.py
+++ testing/eren_christiane/test_maxima.py
@@ -1,14 +1,55 @@
 from maxima import find_maxima
 
+
 def test_simple_sequence():
-	exp = [2,4]
-	x   = [1,2,3,2,4,3]
-	out = find_maxima(x)
-	assert exp == out
+    exp = [2, 4]
+    x = [1, 2, 3, 2, 4, 3]
+    out = find_maxima(x)
+    assert exp == out, 'returns ' + str(out) + ' instead of ' + str(exp)
+
 
 def test_shorter_sequence():
-	exp = [2]	
-	x = [1,2,3]
-	out = find_maxima(x)
-	assert exp ==out
+    exp = [2]
+    x = [1, 2, 3]
+    out = find_maxima(x)
+    assert exp == out, 'returns ' + str(out) + ' instead of ' + str(exp)
 
+
+def test_shorter_sequence2():
+    exp = [0, 3, 5]
+    x = [4, 2, 1, 3, 1, 5]
+    out = find_maxima(x)
+    assert exp == out, 'returns ' + str(out) + ' instead of ' + str(exp)
+
+
+def test_shorter_sequence3():
+    exp = [0, 3]
+    x = [3, 2, 2, 3]
+    out = find_maxima(x)
+    assert exp == out, 'returns ' + str(out) + ' instead of ' + str(exp)
+
+
+def test_shorter_sequence4():
+    exp = [1]
+    x = [1, 3, 2, 2, 1]
+    out = find_maxima(x)
+    assert exp == out, 'returns ' + str(out) + ' instead of ' + str(exp)
+
+
+def test_shorter_sequence5():
+    exp = [3]
+    x = [1, 2, 2, 3, 1]
+    out = find_maxima(x)
+    assert exp == out, 'returns ' + str(out) + ' instead of ' + str(exp)
+
+def test_shorter_sequence6():
+    exp = [2,4]
+    x = [0, 1, 2, 1, 2, 1, 0]
+    out = find_maxima(x)
+    assert exp == out, 'returns ' + str(out) + ' instead of ' + str(exp)
+
+def test_shorter_sequence7():
+    exp = [2]
+    x = [1, 2, 2, 1]
+    out = find_maxima(x)
+    assert exp == out, 'returns ' + str(out) + ' instead of ' + str(exp)
