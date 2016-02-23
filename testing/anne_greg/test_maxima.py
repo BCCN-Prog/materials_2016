9febb5255d2a85fc249845d55bdbc22876526f7e
Anne Song <aeli.song@gmail.com>
2016-02-23 10:46:19 +0100

    fixed test for shorter sequence modify maxima.py for boundary conditions

--- testing/anne_greg/test_maxima.py
+++ testing/anne_greg/test_maxima.py
@@ -4,10 +4,19 @@ def test_simple_sequence():
     x = [1, 2, 3, 2, 4, 3]
     out = find_maxima(x)
     exp = [2, 4]
+    print(out)
     assert exp == out
     
 def test_shorter_sequence():
     x = [1, 2, 3]
     out = find_maxima(x)
     exp = [2]
+    print(out)
+    assert exp == out
+    
+def test_symmetric_sequence():
+    x = [0, 1, 2, 1, 2, 1, 0]
+    out = find_maxima(x)
+    exp = [2, 4]
+    print(out)
     assert exp == out
