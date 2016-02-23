9367aa7358ac8df5991812f8a830ad7b4ad06190
Anne Song <aeli.song@gmail.com>
2016-02-23 10:08:25 +0100

    add test file add test for simple sequence fix: test for shorter sequence

---
+++ testing/anne_greg/test_maxima.py
@@ -0,0 +1,13 @@
+from maxima import find_maxima
+
+def test_simple_sequence():
+    x = [1, 2, 3, 2, 4, 3]
+    out = find_maxima(x)
+    exp = [2, 4]
+    assert exp == out
+    
+def test_shorter_sequence():
+    x = [1, 2, 3]
+    out = find_maxima(x)
+    exp = [2]
+    assert exp == out
