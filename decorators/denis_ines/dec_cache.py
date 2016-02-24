a0a5db68b48f7489d76e70e46c4d06e9fd243768
Denis Alevi <mail@denisalevi.de>
2016-02-24 15:25:20 +0100

    wip

--- decorators/denis_ines/dec_cache.py
+++ decorators/denis_ines/dec_cache.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+import json
 
 def cache(func):
     func._cached = {}
@@ -16,9 +17,44 @@ def cache(func):
             return res
     return wrapper
 
+def cache_to_file(func):
+    def wrapper(*args, **kwargs):
+        all_args = (args, kwargs)
+        arg_hash = hash(pickle.dumps(all_args))
+        try:
+            f = open('cached_{}_results'.format(func.__name__), 'r')
+        except FileNotFoundError:
+            f = open('cached_{}_results'.format(func.__name__), 'w+')
+            print('creating new file')
+
+        try:
+            cached_data = json.load(f)
+            f.close()
+        except ValueError:
+            cached_data = {}
+            f.close()
+
+        if arg_hash in cached_data:
+            print('using cached result')
+            return cached_data[arg_hash]
+        else:
+            print('creating new cache entry')
+            res = func(*args, **kwargs)
+            cached_data[arg_hash] = res
+            with open('cached_{}_results'.format(func.__name__), 'w') as f:
+                json.dump(cached_data, f)
+        return res
+
+    return wrapper
+
 
 @cache
 def sum(x,y, classobject, a=5, b=3):
+    classobject.print_it()
+    return x+y+a+b
+
+@cache_to_file
+def sum_file(x,y, classobject, a=5, b=3):
     classobject.print_it()
     return x+y+a+b
 
@@ -34,5 +70,10 @@ if __name__=='__main__':
     print('sum output:', sum(1,4,d,a=20))
     print('sum output:', sum(1,4,d,a=20))
     print('sum output:', sum(1,4,d,a=20))
+
+    print('sum to file:', sum_file(1,4,d,a=20))
+    print('sum to file:', sum_file(4,4,d,a=20))
+    print('sum to file:', sum_file(1,4,d,a=20))
+
 
 
