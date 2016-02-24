018a17eedd312488c05d6e76393fab889c8176b8
janfb <j.f.boelts@gmail.com>
2016-02-24 16:32:45 +0100

    Final working version of the decorator cache. saves cache to file and reloads after closing the session. takes care of changes to the functions in cache: if they were changed, cache is deleted.“

--- decorators/claudia_jan/cache.py
+++ decorators/claudia_jan/cache.py
@@ -1,5 +1,6 @@
 import functools
 import pickle
+import hashlib
 
 def cache(func):
     # this is called only once
@@ -10,18 +11,27 @@ def cache(func):
         # but use the memory cache for the current session
         func._cache = pickle.load(func._cachefile)
         func._cachefile.seek(0) # seek to top of file
+        # check whether file changed, throw away cache if yes
+        func_string = func.__code__.co_code + pickle.dumps(func.__code__.co_consts) + pickle.dumps(func.__code__.co_varnames)
+        if not(func_string==func._cache[0]):
+            func._cache = {}
+            func._cache[0] = func_string
+            print("Function changed, deleting cache!")
+
     # if there is no cache file, create one
     except FileNotFoundError:
         func._cachefile = open(func.__name__+'_cache.pyc', 'wb+')
         func._cache = {}
+        func._cache[0] = func.__code__.co_code + pickle.dumps(func.__code__.co_consts) + pickle.dumps(func.__code__.co_varnames)
     # if file is empty
     except EOFError:
         func._cache = {}
+        func._cache[0] = func.__code__.co_code + pickle.dumps(func.__code__.co_consts) + pickle.dumps(func.__code__.co_varnames)
 
     def newfunc(*args, **kwargs):
         # try to hash the signature. does not work for lambdas
         try:
-            key = hash(pickle.dumps([args, kwargs.items]))
+            key = hashlib.md5(pickle.dumps([args, kwargs.items])).hexdigest()
         except pickle.PicklingError:
             print("Cant pickle the argument -> no caching. Used a lambda?!..Pfui!")
             return func(*args, **kwargs)
