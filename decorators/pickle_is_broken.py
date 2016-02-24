5075a36b9c679b31a91739dac8856f3c3c38921b
Tiziano Zito <opossumnano@gmail.com>
2016-02-24 15:11:47 +0100

    show why pickle is dangerous for serialization

---
+++ decorators/pickle_is_broken.py
@@ -0,0 +1,42 @@
+import pickle
+FNAME = 'pickle_is_broken.pic'
+
+class A:
+    attr = 10
+
+    def save(self):
+        fh = open(FNAME, 'wb')
+        pickle.dump(self, fh)
+        fh.close()
+
+def test_saving():
+    a = A()
+    a.save()
+    fh = open(FNAME, 'rb')
+    b = pickle.load(fh)
+    fh.close()
+    assert a.attr == b.attr
+
+def test_modify():
+    a = A()
+    a.attr = 100
+    a.save()
+    fh = open(FNAME, 'rb')
+    b = pickle.load(fh)
+    fh.close()
+    assert a.attr == b.attr
+
+def test_back_to_the_future():
+    global A
+    a = A()
+    a.save()
+    # years have passed
+    class A:
+        number = 10
+
+    a = A()
+    fh = open(FNAME, 'rb')
+    b = pickle.load(fh)
+    fh.close()
+    assert a.attr == b.attr
+
