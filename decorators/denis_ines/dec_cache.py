3e6980e7a325665d6a2800b7f776e95500b5cb30
Denis Alevi <mail@denisalevi.de>
2016-02-24 16:32:48 +0100

    not working version of denis cache to file

--- decorators/denis_ines/dec_cache.py
+++ decorators/denis_ines/dec_cache.py
@@ -1,6 +1,8 @@
 import pickle
 import numpy as np
 import json
+import pdb
+import hashlib
 
 def cache(func):
     func._cached = {}
@@ -17,20 +19,26 @@ def cache(func):
             return res
     return wrapper
 
+
 def cache_to_file(func):
     def wrapper(*args, **kwargs):
         all_args = (args, kwargs)
-        arg_hash = hash(pickle.dumps(all_args))
+        arg_hash = hashlib.md5(pickle.dumps(all_args)).hexdigest()
+        print('hash', arg_hash)
         try:
-            f = open('cached_{}_results'.format(func.__name__), 'r')
+            f = open('cached_{}_results'.format(func.__name__), 'r+')
+            print('try1 works')
         except FileNotFoundError:
-            f = open('cached_{}_results'.format(func.__name__), 'w+')
-            print('creating new file')
+            f = open('cached_{}_results'.format(func.__name__), 'w')
+            print('try1 does not work')
+            #print('creating new file')
 
         try:
+            print('try2 works')
             cached_data = json.load(f)
             f.close()
-        except ValueError:
+        except ValueError as e:
+            print('try2 does not work', e)
             cached_data = {}
             f.close()
 
@@ -40,9 +48,11 @@ def cache_to_file(func):
         else:
             print('creating new cache entry')
             res = func(*args, **kwargs)
-            cached_data[arg_hash] = res
+            cached_data[str(arg_hash)] = res
+            print('cached data', cached_data)
             with open('cached_{}_results'.format(func.__name__), 'w') as f:
                 json.dump(cached_data, f)
+                f.flush()
         return res
 
     return wrapper
@@ -52,6 +62,7 @@ def cache_to_file(func):
 def sum(x,y, classobject, a=5, b=3):
     classobject.print_it()
     return x+y+a+b
+
 
 @cache_to_file
 def sum_file(x,y, classobject, a=5, b=3):
@@ -67,13 +78,12 @@ class dummy(object):
 
 if __name__=='__main__':
     d = dummy(np.arange(10000), np.arange(30000))
-    print('sum output:', sum(1,4,d,a=20))
-    print('sum output:', sum(1,4,d,a=20))
-    print('sum output:', sum(1,4,d,a=20))
+#    print('sum output:', sum(1,4,d,a=20))
+#    print('sum output:', sum(1,4,d,a=20))
+#    print('sum output:', sum(1,4,d,a=20))
 
     print('sum to file:', sum_file(1,4,d,a=20))
-    print('sum to file:', sum_file(4,4,d,a=20))
-    print('sum to file:', sum_file(1,4,d,a=20))
+    #print('sum to file:', sum_file(4,4,d,a=20))
 
 
 
