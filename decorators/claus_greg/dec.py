bfbf1f4f443fb5208448f8ff36c7ee4793790f0a
Claus Lang <clauslang@yahoo.de>
2016-02-24 11:07:42 +0100

    ...

--- decorators/claus_greg/dec.py
+++ decorators/claus_greg/dec.py
@@ -1,13 +1,13 @@
-def deprecated(func):
-    if not hasattr(deprecated, 'dict'):
-        deprecated.dict={}
-    deprecated.dict[func.__name__]=False
-    def newfunc(*args, **kwargs):
-        if not deprecated.dict[func.__name__]:
-            print('This function is deprecated')
-            deprecated.dict[func.__name__] = True
+import functools
 
+def deprecated(func):
+    func._seen = False
+    def newfunc(*args, **kwargs):
+        if not func._seen:
+            print('This function is deprecated')
+            func._seen = True
         return func(*args, **kwargs)
+    functools.update_wrapper(newfunc, func)
     return newfunc
 
 @deprecated
@@ -47,3 +47,5 @@ def test_very_special_case():
 for i in range(10):
     print(power(i,2))
     print(add(i,i,i))
+
+print(power.__doc__)
