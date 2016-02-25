9279cb8b5b0adb94dae2f5661fb6964b2ae2a8cf
janfb <j.f.boelts@gmail.com>
2016-02-25 05:59:45 +0100

    Change print messages during caching. Change test script to have more informative print statements.

--- decorators/claudia_jan/test_cache.py
+++ decorators/claudia_jan/test_cache.py
@@ -6,14 +6,31 @@ def test_simple():
     @cache.cache
     def f(x):
         return x+1
+    print('Computes f(1)=2')
     f(1)
+    print('Computes f(2)=3')
     f(2)
+    print('Computes f(1)=2')
     f(1)
 
     print('Changing function, run again')
     @cache.cache
     def f(x):
         return x+2
+    print('Computes f(1)=3')
     f(1)
+    print('Computes f(2)=4')
     f(2)
+    print('Computes f(1)=3')
+    f(1)
+
+    print('Undo changes, run again')
+    @cache.cache
+    def f(x):
+        return x+1
+    print('Computes f(1)=2')
+    f(1)
+    print('Computes f(2)=3')
+    f(2)
+    print('Computes f(1)=2')
     f(1)
